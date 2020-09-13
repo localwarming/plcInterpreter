a5be92f8e5740dd0d6f9026cab9d27a5c6bb9054
isabellabond <ibond713@gmail.com>
2020-09-13 18:01:56 -0400

    Even strings regex

--- src/main/java/plc/interpreter/Regex.java
+++ src/main/java/plc/interpreter/Regex.java
@@ -11,7 +11,7 @@ public class Regex {
     public static final Pattern
             EMAIL = Pattern.compile("[A-Za-z0-9._-]+@[A-Za-z0-9-]*\\.[a-z]{2,3}"),
             FILE_NAMES = Pattern.compile("(?<name>[^\\.]*).*(\\.java|\\.class).*"),
-            EVEN_STRINGS = Pattern.compile(""),
+            EVEN_STRINGS = Pattern.compile("([+-]?\\d+(\\.\\d)?)"),
             INTEGER_LIST = Pattern.compile(""),
             IDENTIFIER = Pattern.compile("([A-Za-z_+\\-*/:!?<>=]+[0-9A-Za-z_+\\-*/:\\.!?<>=]*|\\.[0-9A-Za-z_+\\-*/:\\.!?<>=]+)"),
     //      is -99 an identifier or a number? currently counts as both (also .99 is an identifier technically)
