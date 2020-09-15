e337e2b9c9885d48dcb5d89904b107ba13b98fb0
localwarming <44100146+localwarming@users.noreply.github.com>
2020-09-15 10:04:29 -0400

    Update Regex.java

--- src/main/java/plc/interpreter/Regex.java
+++ src/main/java/plc/interpreter/Regex.java
@@ -10,7 +10,7 @@ public class Regex {
 
     public static final Pattern
             EMAIL = Pattern.compile("[A-Za-z0-9._-]+@[A-Za-z0-9-]*\\.[a-z]{2,3}"),
-            FILE_NAMES = Pattern.compile("(?<name>[^\\.]*).*(\\.java|\\.class).*"),
+            FILE_NAMES = Pattern.compile("(?<name>[^\\.]+).*(\\.java|\\.class).*"),
             EVEN_STRINGS = Pattern.compile("(\\S\\S){5,10}"),
             INTEGER_LIST = Pattern.compile("\\[+\\d?+(?:,+( )?+\\d)*+\\]"),
             IDENTIFIER = Pattern.compile("([A-Za-z_+\\-*/:!?<>=]+[0-9A-Za-z_+\\-*/:\\.!?<>=]*|\\.[0-9A-Za-z_+\\-*/:\\.!?<>=]+)"),
